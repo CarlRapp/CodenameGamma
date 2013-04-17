b24e0513a1bf2c8bb33ee3f2fd776739c36ceab1
Erik Nilsson <nil_erik@hotmail.com>
2013-04-17 23:00:12 +0200

    Can now swap to playscreen

--- CodenameGamma/Screen/MainMenuScreen.cpp
+++ CodenameGamma/Screen/MainMenuScreen.cpp
@@ -5,7 +5,7 @@ MainMenuScreen::MainMenuScreen(ScreenData* Setup)
 	LoadScreenData(Setup);
 
 	
-	gMenuEntries[0]	=	MenuEntry("Play Game",		UNDEFINED_SCREEN);
+	gMenuEntries[0]	=	MenuEntry("Play Game",		PLAY_SCREEN);
 	gMenuEntries[1]	=	MenuEntry("Options",		UNDEFINED_SCREEN);
 	gMenuEntries[2]	=	MenuEntry("Credits",		UNDEFINED_SCREEN);
 	gMenuEntries[3]	=	MenuEntry("Quit",			EXIT_SCREEN);
