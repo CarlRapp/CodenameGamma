22d45ffebc1aedcd8adca9df40f71f8c77e149bc
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-04-30 17:01:16 +0200

    Added GraphicsManager to Level.

--- CodenameGamma/Screen/PlayScreen/Level/Level.cpp
+++ CodenameGamma/Screen/PlayScreen/Level/Level.cpp
@@ -2,15 +2,135 @@
 
 
 Level::Level(){}
-Level::Level(LevelData LData)
+Level::Level(SystemData LData)
 {
+	Effects::InitAll(LData.DEVICE);
+	RenderStates::InitAll(LData.DEVICE);
+	InputLayouts::InitAll(LData.DEVICE);
+	gTextureManager.Init(LData.DEVICE);
+
 	gTerrain	=	new Terrain(LData.DEVICE, LData.DEVICE_CONTEXT);
-	gTerrain->LoadMap("DATA\\Maps\\TestMap");
+
+	gGraphicsManager	=	new GraphicsManager(LData.DEVICE, LData.DEVICE_CONTEXT, LData.RENDER_TARGET_VIEW, LData.SCREEN_WIDTH, LData.SCREEN_HEIGHT);
+	gGraphicsManager->SetTerrain(gTerrain);
+	gGraphicsManager->SetLights(&gDirLights, &gPointLights, &gSpotLights);
+
+
+	AddDirectionalLight(0);
+
+	for( int i = 0; i < 300; ++i)
+	{
+		AddPointLight(0);
+		AddSpotLight(0);
+	}
 }
 
 Level::~Level()
 {
-	gTerrain->~Terrain();
+	if ( gTerrain )
+		delete	gTerrain;
+	if ( gGraphicsManager )
+		delete	gGraphicsManager;
+
+	Effects::DestroyAll();
+	RenderStates::DestroyAll();
+	InputLayouts::DestroyAll();
+}
+
+void Level::LoadLevel(string Levelname)
+{
+	//	TODO:	Fix a more dynamic
+	//			way to load maps.
+	//			I.e. send the extracted
+	//			information to each component
+
+	string	tLevelRootPath	=	"DATA/Maps/";
+
+	LevelData	LData	=	LevelParser::ParseLevel(Levelname, tLevelRootPath);
+
+	if ( LData.IsLoaded() )
+		gTerrain->LoadTerrain(LData);
+}
+
+
+void Level::AddDirectionalLight(DirectionalLight* Instance)
+{
+	float x = MathHelper::RandF(-10.0f, 10.0f);
+	float z = MathHelper::RandF(-10.0f, 10.0f);
+	/*
+	float r = MathHelper::RandF(0.0f, 1.0f);
+	float g = MathHelper::RandF(0.0f, 1.0f);
+	float b = MathHelper::RandF(0.0f, 1.0f);
+	*/
+	
+	float r = 1.0f;
+	float g = 1.0f;
+	float b = 1.0f;
+	
+
+	DirectionalLight *dirLight = new DirectionalLight();
+	dirLight->Color = XMFLOAT4(r,g,b,1);
+	dirLight->Direction = XMFLOAT4(x, -1.0f, z,0);
+
+	gDirLights.push_back(dirLight);
+}
+void Level::AddPointLight(PointLight* Instance)
+{
+	float x = MathHelper::RandF(0.0f, 4000.0f);
+	float z = MathHelper::RandF(0.0f, 4000.0f);
+		
+	float r = MathHelper::RandF(0.0f, 0.6f);
+	float g = MathHelper::RandF(0.0f, 0.6f);
+	float b = MathHelper::RandF(0.0f, 0.6f);
+	/*
+	float r = 1.0f;
+	float g = 1.0f;
+	float b = 1.0f;
+	*/
+	PointLight *pointLight = new PointLight();
+	pointLight->Color		= XMFLOAT4(r, g, b, 0);
+	pointLight->Position	= XMFLOAT3(x, 0.0f, z);
+	pointLight->Range		= 250;
+	gPointLights.push_back(pointLight);
+
+	float Ox = pointLight->Position.x + MathHelper::RandF(-300.0f, 300.0f);
+	float Oz = pointLight->Position.z + MathHelper::RandF(-300.0f, 300.0f);
+
+	rotpos.push_back(XMFLOAT3(Ox, 0.0f, Oz));
+	angle.push_back(MathHelper::RandF(-45.0f, 45.0f));
+}
+void Level::AddSpotLight(SpotLight* Instance)
+{
+	float x = MathHelper::RandF(0.0f, 4000.0f);
+	float z = MathHelper::RandF(0.0f, 4000.0f);
+		
+	float r = MathHelper::RandF(0.0f, 1.0f);
+	float g = MathHelper::RandF(0.0f, 1.0f);
+	float b = MathHelper::RandF(0.0f, 1.0f);
+	/*
+	float r = 1.0f;
+	float g = 1.0f;
+	float b = 1.0f;
+	*/
+
+
+
+	SpotLight *spotLight	= new SpotLight();
+	spotLight->Color		= XMFLOAT4(r, g, b, 0.0f);
+	spotLight->Position		= XMFLOAT3(x, 0, z);
+	spotLight->Direction	= XMFLOAT3(MathHelper::RandF(-1.0f, 1.0f), -1, MathHelper::RandF(-1.0f, 1.0f));
+	spotLight->angle		= XMConvertToRadians(30);
+	spotLight->Range		= 500;
+
+	gSpotLights.push_back(spotLight);
+
+	anglespot.push_back(MathHelper::RandF(-90.0f, 90.0f));
+
+	//float Ox = pointLight->Position.x + MathHelper::RandF(-300.0f, 300.0f);
+	//float Oz = pointLight->Position.z + MathHelper::RandF(-300.0f, 300.0f);
+
+	//rotpos.push_back(XMFLOAT3(Ox, 0.0f, Oz));
+	//angle.push_back(MathHelper::RandF(-45.0f, 45.0f));
 }
 
 
@@ -20,10 +140,67 @@ Level::~Level()
 
 
 
+void Level::Update(float DeltaTime)
+{
+
+	for (int i = 0; i < (int)gPointLights.size(); ++i)
+	{
+		PointLight *pLight = gPointLights[i];
+
+		XMFLOAT3 rp = rotpos[i];
+		float a		= angle[i];
 
 
+		XMVECTOR pos  = XMLoadFloat3(&pLight->Position);
+		//XMVECTOR move = XMLoadFloat3(&XMFLOAT3(0,0,0.1f));
+
+		
+		XMMATRIX transl1 = XMMatrixTranslation(-rp.x, -2000.0f, -rp.z);
+		XMMATRIX transl2 = XMMatrixTranslation(rp.x, 2000.0f, rp.z);	
+		XMMATRIX rot	 = XMMatrixRotationY(DeltaTime * XMConvertToRadians(a));
+
+		XMMATRIX transf = transl1 * rot * transl2;
 
 
+		pos = XMVector3TransformCoord(pos, transf);
+
+		//pos = pos + move;
+
+		XMStoreFloat3(&pLight->Position, pos);
+
+		//pLight->Position.z -= 0.1f;
+		pLight->Position.y = gTerrain->GetHeight(pLight->Position.x, pLight->Position.z) + 50;
+	}
+
+	for (int i = 0; i < (int)gSpotLights.size(); ++i)
+	{
+		SpotLight *sLight = gSpotLights[i];
+
+		float a		= anglespot[i];
+
+		XMVECTOR dir  = XMLoadFloat3(&sLight->Direction);
+
+		//XMMATRIX transl1 = XMMatrixTranslation(-sLight->Position.x, -2000.0f, -sLight->Position.z);
+		//XMMATRIX transl2 = XMMatrixTranslation(sLight->Position.x, 2000.0f, sLight->Position.z);
+		XMMATRIX rot	 = XMMatrixRotationY(DeltaTime * XMConvertToRadians(a));
+
+		dir = XMVector3TransformCoord(dir, rot);
+
+		XMStoreFloat3(&sLight->Direction, dir);
+		//XMMATRIX transf = transl1 * rot * transl2;
+
+		sLight->Position.y = gTerrain->GetHeight(sLight->Position.x, sLight->Position.z) + 200;
+	}
+
+}
+void Level::Render()
+{
+	
+}
+void Level::Render(vector<Player*>& players)
+{
+	gGraphicsManager->Render(players);
+}
 
 
 
--- CodenameGamma/Screen/PlayScreen/Level/Level.h
+++ CodenameGamma/Screen/PlayScreen/Level/Level.h
@@ -3,29 +3,57 @@
 #define LEVEL_H
 
 #include "../../../stdafx.h"
+#include "../Graphics/Effects.h"
+#include "../Graphics/RenderStates.h"
+#include "../Graphics/Buffer.h"
+#include "../Graphics/TextureManager.h"
+#include "../Graphics/GraphicsManager.h"
 #include "../Terrain/Terrain.h"
+#include "LevelParser.h"
 
-struct LevelData
+struct SystemData
 {
-	LevelData(){}
+	SystemData(){}
 
 	ID3D11Device*			DEVICE;
 	ID3D11DeviceContext*	DEVICE_CONTEXT;
+	ID3D11RenderTargetView*	RENDER_TARGET_VIEW;
+	int	SCREEN_WIDTH, SCREEN_HEIGHT;
 };
 
+using namespace std;
 class Level
 {
 private:
-	Terrain*	gTerrain;
+	TextureManager		gTextureManager;
+
+	GraphicsManager*	gGraphicsManager;
+	Terrain*			gTerrain;
+
+	//	Lights
+	vector<DirectionalLight*>	gDirLights;
+	vector<PointLight*>			gPointLights;
+	vector<SpotLight*>			gSpotLights;
+
+	vector<XMFLOAT3> rotpos;
+	vector<float>	 angle;
+	vector<float>	 anglespot;
+
+
+	void	AddDirectionalLight(DirectionalLight* Instance);
+	void	AddPointLight(PointLight* Instance);
+	void	AddSpotLight(SpotLight* Instance);
 
 public:
 	Level(void);
-	Level(LevelData Data);
+	Level(SystemData Data);
 	~Level(void);
+
+	void	LoadLevel(string Levelname);
 
 	void	Update(float DeltaTime);
 	void	Render();
-
+	void	Render(vector<Player*>& players);
 
 	Terrain*	GetTerrain();
 };
