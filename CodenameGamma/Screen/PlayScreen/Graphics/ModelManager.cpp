931a063bf44d37d9eb09ff98de78874c6d14137f
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-05-06 16:02:00 +0200

    Added ModelManager (Gets deleted when application is closed)

---
+++ CodenameGamma/Screen/PlayScreen/Graphics/ModelManager.cpp
@@ -0,0 +1,55 @@
+#include "ModelManager.h"
+
+ModelManager* ModelManager::gInstance	=	0;
+ModelManager::ModelManager(ID3D11Device* Device)
+{
+	gLoadedModels	=	map<string, Model*>();
+	gDevice			=	Device;
+
+	gTextureManager.Init(Device);
+}
+ModelManager::~ModelManager()
+{
+	for ( gModelIterator = gLoadedModels.begin(); gModelIterator != gLoadedModels.end(); ++gModelIterator )
+		delete	gModelIterator->second;
+
+	gLoadedModels.clear();
+}
+
+bool ModelManager::IsInitialized()
+{
+	return gInstance != 0;
+}
+
+void ModelManager::Initialize(ID3D11Device* Device)
+{
+	if ( IsInitialized() )
+		return;
+
+	gInstance		=	new ModelManager(Device);
+}
+
+ModelManager* ModelManager::GetInstance()
+{
+	return gInstance;
+}
+
+void ModelManager::LoadModel(string Name, string Filename, string Path)
+{
+	if ( gLoadedModels.count(Name) != 0 )
+		return;
+
+	Model*	tModel	=	new Model(gDevice, gTextureManager, Path + Filename, Path);
+	gLoadedModels.insert(pair<string, Model*>(Name, tModel));
+
+	DebugScreen::GetInstance()->AddLogMessage("Model: \"" + Filename + "\" loaded.", Green);
+}
+
+Model* ModelManager::GetModel(string Name)
+{
+	if ( gLoadedModels.count(Name) != 0)
+		return gLoadedModels[Name];
+
+	DebugScreen::GetInstance()->AddLogMessage("Model: \"" + Name + "\" is not loaded.", Red);
+	return 0;
+}
