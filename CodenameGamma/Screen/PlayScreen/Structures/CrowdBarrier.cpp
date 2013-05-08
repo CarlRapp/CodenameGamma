87d179b1288ff7c2637ed68b35005838d1be13b9
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-05-08 17:05:09 +0200

    Updated model, new scale.

--- CodenameGamma/Screen/PlayScreen/Structures/CrowdBarrier.cpp
+++ CodenameGamma/Screen/PlayScreen/Structures/CrowdBarrier.cpp
@@ -3,7 +3,6 @@
 CrowdBarrier::CrowdBarrier()
 {
 	SetModelInstance( ModelManager::GetInstance()->CreateModelInstance( "CrowdBarrier" ) );
-	SetScale( XMFLOAT3( 10, 5, 10 ) );
 }
 
 CrowdBarrier::~CrowdBarrier()
