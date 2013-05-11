5bba652274e19abdb1884ab2818fd71196b4e5c3
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-05-11 14:24:03 +0200

    Added items (Things you can pickup from the ground)

---
+++ CodenameGamma/Screen/PlayScreen/Items/Item.cpp
@@ -0,0 +1,31 @@
+#include "Item.h"
+
+Item::Item()
+{
+
+}
+
+Item::~Item()
+{
+
+}
+
+bool Item::Update(float DeltaTime, Terrain* terrain)
+{
+
+	return GameObject::Update(DeltaTime, terrain);
+}
+
+void Item::CollideWith(GameObject* Instance)
+{
+	if ( !IsOfType<Unit>(Instance) )
+		return;
+
+
+	OnPickUp( ((Unit*)Instance) );
+}
+
+void Item::OnPickUp(Unit* Instance)
+{
+
+}
