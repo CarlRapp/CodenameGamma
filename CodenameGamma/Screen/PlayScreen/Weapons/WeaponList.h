ed9c3b1cddb7ad3dc433273db7715ad375633012
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-05-12 19:40:03 +0200

    Added a headerfile containing all weapons and projectiles to more easily include them

---
+++ CodenameGamma/Screen/PlayScreen/Weapons/WeaponList.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef WEAPONLIST_H
+#define WEAPONLIST_H
+
+#include "Weapon.h"
+#include "Projectiles/Projectile.h"
+
+//	Pistols
+#include "Pistol.h"
+
+//	Rifles
+#include "Shotgun.h"
+
+//	Projectiles
+#include "Projectiles/Bullet.h"
+
+#endif
