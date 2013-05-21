0a993f4286e08d4efcd8342516f2ca94f6008990
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-05-21 20:18:45 +0200

    Early version of Health Bar ( Ingen snygg kod atm )

--- CodenameGamma/Screen/PlayScreen/PlayScreen.cpp
+++ CodenameGamma/Screen/PlayScreen/PlayScreen.cpp
@@ -22,7 +22,11 @@ PlayScreen::PlayScreen(ScreenData* Setup)
 #pragma region Load / Unload
 bool PlayScreen::Load()
 {
-
+	for( int n = 0; n < 10; ++n)
+	{
+		string	tPath	=	"DATA/GUI/Health/Health_" + to_string( (long double) n + 1 ) + ".png";
+		D3DX11CreateShaderResourceViewFromFile( gScreenData->DEVICE, tPath.c_str(), 0, 0, &gHealthBar[n], 0 );
+	}
 	return true;
 }
 
@@ -31,6 +35,11 @@ bool PlayScreen::Unload()
 	
 	if ( gLevel )
 		delete	gLevel;
+
+	for( int n = 0; n < 10; ++n)
+	{
+		gHealthBar[n]->Release();
+	}
 		
 	return true;
 }
@@ -52,13 +61,20 @@ void PlayScreen::Update(float DeltaTime)
 
 		gGotoNextFrame	=	POST_PLAY_SCREEN;
 	}
+
+	if( InputManager::GetInstance()->GetController(0)->GetButtonState( A ) == PRESSED )
+		if( gLevel->GetPlayers()[0]->GetUnit() )
+			gLevel->GetPlayers()[0]->GetUnit()->Hurt( 10 );
 }
 
 void PlayScreen::Render()
 {
 	gLevel->Render();
 
+	for each( Player* p in gLevel->GetPlayers() )
+		RenderGUI( p );
 
+	/*
 	for each( Player* p in gLevel->GetPlayers() )
 	{
 		PlayerUnit*	tUnit	=	p->GetUnit();
@@ -103,7 +119,7 @@ void PlayScreen::Render()
 				0
 			);
 		}
-	}
+	}*/
 }
 
 ScreenType PlayScreen::GetScreenType()
@@ -119,7 +135,37 @@ void PlayScreen::Reset()
 
 void PlayScreen::RenderGUI( Player* P )
 {
+	if( !P->GetUnit() )
+		return;
 
 	GraphicsManager*	GM	=	gLevel->GetGraphicsManager();
+	D3D11_VIEWPORT		pVP	=	P->GetCamera()->GetViewPort();
 
+	D3D11_VIEWPORT	tVP;
+	tVP.TopLeftX	=	pVP.TopLeftX;
+	tVP.TopLeftY	=	pVP.TopLeftY;
+	tVP.MinDepth	=	0.0f;
+	tVP.MaxDepth	=	1.0f;
+	tVP.Width		=	70;
+	tVP.Height		=	58;
+
+	UnitHealth	uHealth	=	P->GetUnit()->GetHealth();
+	int HP		=	(int)( 100.0f * (uHealth.first/uHealth.second) );
+	int	index	=	HP / 10;
+	if( index > 0 )
+	{
+		GM->RenderQuad( tVP, gHealthBar[index - 1], Effects::CombineFinalFX->AlphaClipColorTech );
+
+		DrawString(
+			*gTextInstance,
+			to_string( (long double)HP ),
+			tVP.TopLeftX + tVP.Width * 0.5f,
+			tVP.TopLeftY + tVP.Height * 0.5f,
+			15,
+			White,
+			Black,
+			1.0f,
+			FW1_CENTER | FW1_VCENTER
+		);
+	}
 }
