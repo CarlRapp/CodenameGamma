e97429369a27124de8d79b47957ac08668b80139
Erik Nilsson <nil_erik@hotmail.com>
2013-06-01 22:27:09 +0200

    Added new weapons

---
+++ CodenameGamma/Screen/PlayScreen/Weapons/AutomaticRifle.cpp
@@ -0,0 +1,78 @@
+#include "AutomaticRifle.h"
+#include "../../ScreenEnums.h"
+
+AutomaticRifle::AutomaticRifle()
+{
+	SetModelInstance( ModelManager::GetInstance()->CreateModelInstance( "AutomaticRifle" ) );
+	SetWOGModelInstance( ModelManager::GetInstance()->CreateModelInstance( "AutomaticRifle-WOG" ) );
+
+	gCooldown	=	WeaponCooldown(0.0f, 1.0f);
+	gClip		=	WeaponClip(8, 8);
+	gAmmo		=	16;
+
+	gReloadTime	=	WeaponReloadTime(0.0f, 4.0f);
+
+	gWeaponAnimations.Aim				= "RifleAim";
+	gWeaponAnimations.Draw				= "WeaponDraw";
+	gWeaponAnimations.DrawReloadPutAway = "RifleDrawReloadPutAway";
+	gWeaponAnimations.PutAway			= "WeaponPutAway";
+	gWeaponAnimations.Shoot				= "RifleShoot";
+	gWeaponAnimations.Reload			= "RifleReload";
+	gWeaponAnimations.UpperWalk			= "WeaponUpperWalk";
+	gWeaponAnimations.UpperRun			= "WeaponUpperRun";
+	gWeaponAnimations.UpperStand		= "WeaponUpperStand";
+
+	gReloadSound = "Shotgun_Reload";
+}
+
+AutomaticRifle::~AutomaticRifle()
+{
+
+}
+
+bool AutomaticRifle::CanFire()
+{
+	return (gClip.first > 0 && gCooldown.first <= 0);
+}
+
+bool AutomaticRifle::Fire( GameObject* Owner, GameObject* Target, float DamageMul )
+{
+	if ( CanFire() )
+	{
+		Bullet*	tBullet	=	new Bullet();
+		tBullet->MultiplyDamage( DamageMul );
+
+		XMVECTOR	tVelocityV	= tBullet->GetSpeed() * XMVector3Normalize( XMLoadFloat3( &GetFloat3Value(Direction) ) );
+		XMFLOAT3	tVelocity	=	GetFloat3Value( Direction );
+		XMStoreFloat3(&tVelocity, tVelocityV);
+
+		tBullet->SetRotation( GetQuaternation() );
+
+		XMFLOAT3 pipePos;
+
+		if (GetJointPosition("Pipe", pipePos))
+			tBullet->MoveTo( pipePos );
+		else
+			tBullet->MoveTo( GetFloat3Value( Position ) );
+
+		tBullet->SetVelocity( tVelocity );
+		tBullet->SetTeam( Owner->GetTeam() );
+		tBullet->SetOwner( Owner );
+
+		SoundManager::GetInstance()->Play("Shotgun_Fire", SFX);
+
+		gCooldown.first	=	gCooldown.second;
+		--gClip.first;
+
+		AddGameObject( tBullet );
+
+		return true;
+	}
+	else if ( gClip.first == 0 && gCooldown.first <= 0)
+	{
+		
+		gCooldown.first	=	gCooldown.second;
+	}
+
+	return false;
+}
